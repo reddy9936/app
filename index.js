b9aeadccc1fa313d6e72a3a2b36f08e3e5062bb7
Venkat <venkatreddygutha1@gmail.com>
2023-08-01 22:56:01 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const config = require("config");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const express = require("express");
+const app = express();
+console.log(config.get("jwtPrivateKey"));
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
